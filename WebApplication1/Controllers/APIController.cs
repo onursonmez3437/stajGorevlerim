d813483b2c889980654b147705ded712887178e0
onur sonmez <onursonmez3437@gmail.com>
2024-11-27 22:51:14 +0300

    proje gıthuba eklendi

---
+++ WebApplication1/Controllers/APIController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models.siniflar;
+
+namespace WebApplication1.Controllers
+{
+    public class APIController : Controller
+    {
+
+        Context c = new Context();
+        // GET: API
+        public ActionResult Index()
+        {
+            var t = c.Nedir.ToList();
+            return View(t);
+        }
+
+        public ActionResult nedir()
+        {
+            var t  = c.Nedir.ToList();
+            return View(t);
+        }
+    }
+}
---
+++ WebApplication1/Models/siniflar/Nedir.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.siniflar
+{
+	public class Nedir
+	{
+		[Key]
+		public int ID { get; set; }
+		public string konu { get; set; }
+		public string aciklama { get; set; }
+        public string ekAciklama { get; set; }
+        public int deger { get; set; }
+
+    }
+}
